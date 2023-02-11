bbe7a93f7430b0a1a77612151ecaaf1fc8546ef7
Samuel Asher Rivello <sam@RivelloMultimediaConsulting.com>
2023-02-11 09:38:32 +0200

    Copy in implementation from RMC Mini MVCS for parity

--- Runtime/Core/Scripts/Runtime/RMC/Core/Data/Types/Observables/Observable.cs
+++ Runtime/Core/Scripts/Runtime/RMC/Core/Data/Types/Observables/Observable.cs
@@ -1,20 +1,60 @@
-﻿
+﻿using UnityEngine.Events;
+
 namespace RMC.Core.Data.Types.Observables
 {
     /// <summary>
-    /// Wrapper so a type can be observable via events
+    /// The <see cref="UnityEvent"/> for <see cref="Observable{T}"/>.
     /// </summary>
-    public class Observable<T> 
+    public class ObservableUnityEvent<T, U> : UnityEvent<T, U>
+    {
+        //  Fields ----------------------------------------
+        private Observable<T> _observable;
+        
+        
+        //  Constructor Methods ---------------------------
+        public ObservableUnityEvent(Observable<T> observable) : base()
+        {
+            _observable = observable;
+        }
+        
+        
+        //  Methods ---------------------------------------
+        public new void AddListener(UnityAction<T, U> call)
+        {
+            AddListener(call, false);
+        }
+        
+        /// <summary>
+        /// Consuming scope can optionally 'catch up' to the existing
+        /// value by immediately refreshing value to the observer
+        /// </summary>
+        /// <param name="call"></param>
+        /// <param name="willRefreshImmediately"></param>
+        public void AddListener(UnityAction<T, U> call, bool willRefreshImmediately )
+        {
+            base.AddListener(call);
+            if (willRefreshImmediately)
+            {
+                _observable.OnValueChangedRefresh();
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Wrapper where any changes to value of type
+    /// <see cref="TValue"/> can be observable via events
+    /// </summary>
+    public class Observable<TValue> 
     {
         //  Events ----------------------------------------
-        public readonly ObservableUnityEvent<T,T> OnValueChanged = new ObservableUnityEvent<T,T>();
+        public readonly ObservableUnityEvent<TValue, TValue> OnValueChanged;
 
         //  Properties ------------------------------------
         
         /// <summary>
         /// Keep this property name as "Value"
         /// </summary>
-        public T Value
+        public TValue Value
         {
             set
             {
@@ -28,30 +68,38 @@ namespace RMC.Core.Data.Types.Observables
         }
 
         //  Fields ----------------------------------------
-        private T _currentValue;
-        private T _previousValue;
+        private TValue _currentValue;
+        private TValue _previousValue;
 
         //  Constructor Methods ---------------------------
-        static Observable()
-        {
-            //Debug.LogWarning("Observable static called. FYI to me");
-        }
 
         public Observable()
         {
-            //This gets called on scenes start. Cool. Useful?
-            //Debug.LogWarning("Observable constr called. FYI to me");
+            OnValueChanged = new ObservableUnityEvent<TValue,TValue>(this);
         }
 
         //  Methods ---------------------------------------
-        protected virtual T OnValueChanging(T previousValue, T newValue)
+        
+        /// <summary>
+        /// Refresh the observers by recalling Invoke()
+        /// with the latest values
+        /// </summary>
+        public void OnValueChangedRefresh()
+        {
+            OnValueChanged.Invoke(_previousValue, _currentValue);
+        }
+        
+        protected virtual TValue OnValueChanging(TValue previousValue, TValue newValue)
         {
             //Optional: Override method to gate/police the value changes
             _previousValue = previousValue;
             return newValue;
         }
+        
+        
 
         //  Event Handlers --------------------------------
+  
     }
 
 }
